4cf6917293bbb82665f5e633006299d6a59ec5b9
David Dobre <daviddobre@gmail.com>
2018-11-20 15:40:44 -0800

    WIP: trying to figure out what is causing async http requests to fail on larger datasets

--- static_analysis/2-scrape_js/async_js_get.py
+++ static_analysis/2-scrape_js/async_js_get.py
@@ -6,6 +6,9 @@
 # Script adapted by David Dobre Nov 14 '18:
 #       Added parquet loading, iteration over dataframes, and content saving
 ################################################################################
+from pathlib import Path
+import os
+import os.path
 import pandas as pd
 import concurrent.futures
 import requests
@@ -15,21 +18,31 @@ import time
 # Parameters and input
 
 out = []
-CONNECTIONS = 20
+CONNECTIONS = 50
 TIMEOUT = 2
 
-#MAIN_DIR    = '/media/ddobre/UCOSP_DATA/'
-MAIN_DIR    = '/mnt/Data/UCOSP_DATA/'
+#STORAGE_DIR = '/media/ddobre/UCOSP_DATA/'
+STORAGE_DIR = '/mnt/Data/UCOSP_DATA'
 
-OUTPUT_DIR  = MAIN_DIR + 'js_source_files/'
+PWD = os.path.dirname(os.path.realpath(__file__))
+OUTPUT_DIR  = os.path.join(STORAGE_DIR, "js_source_files/")
+
+LOG_GETS    = os.path.join(PWD, "gets.log")
+LOG_WRITES  = os.path.join(PWD, "writes.log")
+LOG_EXCEPTS = os.path.join(PWD, "excepts.log")
+
+log_gets_file = open(LOG_GETS, "a+")
+log_writes_file = open(LOG_WRITES, "a+")
+log_excepts_file = open(LOG_EXCEPTS, "a+")
 
 #### Small sample
-#URL_LIST = MAIN_DIR + 'resources/url_master_list.csv'
+#URL_LIST = STORAGE_DIR + 'resources/url_master_list.csv'
 #input_data = pd.read_csv(URL_LIST);
 #input_data['script_url'] = input_data['url'] # just for laziness
 
 #### Larger dataset
-URL_LIST = MAIN_DIR + 'resources/sample_full_url_list_test/'
+#URL_LIST = STORAGE_DIR + 'resources/sample_full_url_list_test/'
+URL_LIST = os.path.join(STORAGE_DIR, "resources/full_url_list_v2/")
 
 from pathlib import Path
 PARQUET_DIR = Path(URL_LIST)
@@ -37,11 +50,43 @@ input_data = pd.concat(
     pd.read_parquet(parquet_file) for parquet_file in PARQUET_DIR.glob('*.parquet')
 )
 
+print("Expecting {} url connections".format(input_data.shape))
+
 ################################################################################
 # Handle a URL
+'''Current attempt with exception handling'''
 def load_url(url, filename, timeout):
+
+    try:
+        response = requests.get(url, timeout=timeout)
+
+#        print("{}\t{}".format(response.status_code, url))
+
+        # This is probably very dangerous, having many workers writing to one file
+        # Does this implementation have built in synchronization?
+        log_gets_file.write("{}\t{}\n".format(response.status_code, url))
+
+        if response.status_code == 200:
+            content = response.text
+#            print("\t\t\t{}".format(len(content)))
+#            sys.stdout.write("[%s-%d]]\n" % (url, len(content)))
+#            sys.stdout.flush()
+            return response.status_code, content, filename
+
+    except requests.exceptions.RequestException as e:
+        return -1, e, filename
+
+    # Don't forget to cast status code to int. Computers are the suck.
+    return int(response.status_code), "", filename
+
+
+'''
+def load_url(url, filename, timeout):
+
     response = requests.get(url, timeout=timeout)
-#    print("{}\t{}".format(response.status_code, url))
+
+    #print("{}\t{}".format(response.status_code, url))
+    log_gets_file.write("{}\t{}\n".format(response.status_code, url))
 
     if response.status_code == 200:
         content = response.text
@@ -52,18 +97,29 @@ def load_url(url, filename, timeout):
 
     # Don't forget to cast status code to int. Computers are the suck.
     return int(response.status_code), "", filename
+'''
 
-#def load_url(url, filename, timeout):
-#
-#    ans = requests.head(url, timeout=timeout)
-#
-#    if (ans.status_code == 200):
-#        content = requests.get(url).text
-#    else:
-#        content = ""
-#
-#    return ans.status_code, content, filename
+''' Some exception examples'''
+# except requests.exceptions.Timeout:
+#     # Maybe set up for a retry, or continue in a retry loop
+# except requests.exceptions.TooManyRedirects:
+#     # Tell the user their URL was bad and try a different one
+# except requests.exceptions.RequestException as e:
 
+
+''' Old function'''
+'''
+def load_url(url, filename, timeout):
+
+    ans = requests.head(url, timeout=timeout)
+
+    if (ans.status_code == 200):
+        content = requests.get(url).text
+    else:
+        content = ""
+
+    return ans.status_code, content, filename
+'''
 
 ################################################################################
 # Launch workers
@@ -79,31 +135,39 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=CONNECTIONS) as executor:
 
     for future in concurrent.futures.as_completed(future_to_url):
         http_status, content, filename = future.result()
+
         if http_status == 200 and content:
-            print(filename)
+#            print("\n\n")
+#            print(filename)
+#            print("\n\n")
+            log_writes_file.write("{}\t\t{}\n".format(len(content),filename))
+
             with open(filename, "w") as source_file:
                 source_file.write(content)
 
-#    for future in concurrent.futures.as_completed(future_to_url):
-#        try:
-#            data, content, filename = future.result()
-#
-#
-#            if (data == 200 and content):
-#                with open(filename, 'w') as source_file:
-#                    source_file.write(content)
-#
-#        except Exception as exc:
-#            data = str(type(exc))
-#
-#        finally:
-#            out.append(data)
-#
-#            # Print out current count
-#            print(str(len(out)),end="\r")
+        elif http_status == -1:
+            log_excepts_file.write("{}:\t\t{}\n".format(filename,content))
+
+'''
+    for future in concurrent.futures.as_completed(future_to_url):
+        try:
+            data, content, filename = future.result()
+
+            if (data == 200 and content):
+                with open(filename, 'w') as source_file:
+                    source_file.write(content)
+
+        except Exception as exc:
+            data = str(type(exc))
+
+        finally:
+            out.append(data)
+
+            # Print out current count
+            print(str(len(out)),end="\r")
 
     time2 = time.time()
-
+'''
 
 ################################################################################
 # Summary
