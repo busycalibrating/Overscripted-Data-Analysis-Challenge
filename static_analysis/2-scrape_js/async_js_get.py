58b1ca53f1f292872684a9470c0b0c2a6112b853
David Dobre <daviddobre@gmail.com>
2018-11-28 21:20:54 -0800

    Tidied up scraper, implemented directory checking to prevent duplicate requests for existing files

--- static_analysis/2-scrape_js/async_js_get.py
+++ static_analysis/2-scrape_js/async_js_get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Original code: cgarcoae, Sep 21 '18
+# Original code: Cristian Garcia, Sep 21 '18
 #       https://medium.com/@cgarciae/making-an-infinite-number-of-requests-with-
 #               python-aiohttp-pypeln-3a552b97dc95
 #
@@ -13,56 +13,65 @@
 #
 ################################################################################
 
-import aiohttp
-from aiohttp import ClientSession, TCPConnector
+from aiohttp import ClientError, ClientSession, TCPConnector
 import asyncio
 import os, os.path
 import pandas as pd
 import ssl
 import sys
+import glob
 
 from pathlib import Path
 from pypeln import asyncio_task as aio
 
 ##### Specify Directories ######################################################
+# Parent directory
+STORAGE_DIR = "/mnt/Data/UCOSP_DATA"
+
+# Input directory
+URL_LIST = os.path.join(STORAGE_DIR, "resources/full_url_list_parsed/")
+
+# Output directory
+OUTPUT_DIR = os.path.join(STORAGE_DIR, "js_source_files/")
+
 # Max number of workers
-limit = 20
+LIMIT = 20 # play with this value, higher values fail for me
 
 # Not sure what this monkey business does
 ssl.match_hostname = lambda cert, hostname: True
 
-# Parent directories
-storage_dir = "/mnt/Data/UCOSP_DATA"
-pwd = os.path.dirname(os.path.realpath(__file__))
-
-# Input directory
-#url_list = os.path.join(storage_dir, "resources/sample_full_url_list_test/")
-url_list = os.path.join(storage_dir, "resources/full_url_list_v2/")
-
-# Output directory
-output_dir = os.path.join(storage_dir, "js_source_files/")
-
 
 ##### Load in dataset ##########################################################
-parquet_dir = Path(url_list)
-
+parquet_dir = Path(URL_LIST)
 input_data = pd.concat(
     pd.read_parquet(parquet_file)
-        for parquet_file in parquet_dir.glob('*.parquet')
+    for parquet_file in parquet_dir.glob('*.parquet')
 )
 
 # Sanity check
-print("Expecting {} url connections".format(input_data.shape[0]))
+print("There are {} urls found in the dataset.".format(input_data.shape[0]))
 
-input_data['filename'] = output_dir + input_data['filename']
+# Check for existing files in output directory
+existing_files = [os.path.basename(x) for x in glob.glob(OUTPUT_DIR + "*.txt")]
+print("There are {} existing files in {}.".format(
+                                                len(existing_files),
+                                                OUTPUT_DIR)
+                                            )
+
+# Remove those from the "to request" list as they're already downloaded
+input_data = input_data[~input_data['filename'].isin(existing_files)]
+
+# Append filename to the output folder and get the raw string value
+input_data['filename'] = OUTPUT_DIR + input_data['filename']
 input_data = input_data.values
+print("> {} urls left to request.".format(input_data.shape[0]))
 
 
 ##### Async fetch ##############################################################
 async def fetch(data, session):
 
-    url = data[0]
-    filename = data[1]
+    url = data[1]
+    filename = data[2]
 
     try:
         async with session.get(url, timeout=2) as response:
@@ -70,17 +79,16 @@ async def fetch(data, session):
             print("{}\t{}".format(response.status, url))
 
             if (response.status == 200 and output):
-
                 print("\tsuccess")
 
                 with open(filename, "wb") as source_file:
                     source_file.write(output)
-
                 return output
 
             return response.status
 
-    except aiohttp.ClientError as e:
+    # Catch exceptions
+    except ClientError as e:
         print(e)
         return e
 
@@ -111,12 +119,13 @@ async def fetch(data, session):
 
 ##### Iterate over each list entry #############################################
 aio.each(
-    fetch,
-    input_data,
-    workers = limit,
+    fetch,              # worker function
+    input_data,         # input arguments
+    workers = LIMIT,    # max number of workers
     on_start = lambda: ClientSession(connector=TCPConnector(limit=None)),
     on_done = lambda _status, session: session.close(),
     run = True,
 )
 
-print("\n\nDONE")
+print(80*"#")
+print("\nDONE")
