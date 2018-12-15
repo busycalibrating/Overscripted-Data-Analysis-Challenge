24a8e8448aa157bd38dfbcefaa9cc4e60d24151e
David Dobre <ddobre@sfu.ca>
2018-12-15 15:43:36 -0800

    pt3 done

--- static_analysis/3-generate_symbols_of_interest/process_APIs.py
+++ static_analysis/3-generate_symbols_of_interest/process_APIs.py
@@ -1,63 +1,22 @@
 #!/usr/bin/env python3
-from os import listdir
+import configparser
 from csv import DictWriter
 import json
+from os import listdir
 import sys
 
-
-################################################################################
-# sanity check (ensure correct syntax)
-def checkNumberOfInputs():
-
-    # check correct inputs
-    if (len(sys.argv) != 3):
-        print('''Warning: invalid input type!
-Syntax:
-        $ python3.6 this_script.py <list_of_json_files.txt> \
-<file_location_dir>
-OR
-        $ python3.6 this_script.py <path/to/json_file.json> \
-<file_location_dir>
-''')
-        exit()
-    return
-
-
-################################################################################
-# check input type (txt: list of json files OR single path/to/file.json)
-def inputIsFilelist():
-    state = False
-
-    # get file extension
-    filetype = sys.argv[1].split(".")[-1]
-
-    # check for types
-    if (filetype == 'txt'):
-        # if txt, must loop over entries
-        state = True
-
-    # fail if type is not txt or json
-    elif ((filetype != 'json') and (filetype != 'JSON')):
-        print("Warning: invalid input type! Need json or txt file")
-        exit()
-
-    return state
-
-
-################################################################################
 # want to return a dict (lowercase : FileName.json) to recursively get nested
 #   methods and properties.
-def getAllFileDict():
+def getAllFileDict(api_data):
     master_API_dict = {}
 
-    for entry in listdir(sys.argv[2]):
+    for entry in listdir(api_data):
         key = str(entry.split(".")[0])
         master_API_dict[key] = entry
 
     return master_API_dict
 
 
-################################################################################
 # import all data from specified filename
 def importData(filename):
 
@@ -68,7 +27,6 @@ def importData(filename):
     return data['api']
 
 
-################################################################################
 # once imported data from file, extract the desired properties
 def extractProperties(json_data):
 
@@ -81,7 +39,6 @@ def extractProperties(json_data):
     return str(interface_name), property_list
 
 
-################################################################################
 def recursivelyGetProperties(current_interface, res_dict, master_API_dict):
 
     # import data from file
@@ -107,44 +64,36 @@ def recursivelyGetProperties(current_interface, res_dict, master_API_dict):
                 new_seed = sys.argv[2] + master_API_dict[entry]
                 res_dict = recursivelyGetProperties(new_seed, res_dict, \
                         master_API_dict)
-
     return res_dict
 
-
-################################################################################
 def main():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
 
-    # sanity check
-    checkNumberOfInputs()
+    seed_apis = config['DEFAULT']['seed']
+    api_data = config['DEFAULT']['api_data']
+    output = config['DEFAULT']['output']
 
     # init empty dict to store all results
     res_dict = {}
 
     # read in all available files to recurse over
-    master_API_dict = getAllFileDict()
+    master_API_dict = getAllFileDict(api_data)
 
-    # check input type
-    if (inputIsFilelist()):
-        file_list = open(sys.argv[1]).read().splitlines()
+    # open seed file
+    file_list = open(seed_apis).read().splitlines()
 
-        # iterate over the list of specified files
-        for entry in file_list:
+    # iterate over the list of specified files
+    for entry in file_list:
 
-            # generate a nice filename
-            file_location = sys.argv[2] + entry
-            res_dict = recursivelyGetProperties(file_location, res_dict, \
-                    master_API_dict)
-
-    else:
-        # with only one specified file, only need to call getProperties once
-        res_dict = recursivelyGetProperties(sys.argv[1], res_dict, \
+        # generate a nice filename
+        file_location = api_data + entry
+        res_dict = recursivelyGetProperties(file_location, res_dict, \
                 master_API_dict)
 
     # dump all results to a file
-    with open('symbol_list.json', 'w') as fp:
+    with open(output, 'w') as fp:
         json.dump(res_dict, fp, indent=4)
 
-
-################################################################################
 if __name__ == '__main__':
     main();
